6ba79a63edb7cbd1447e2c57a4990d0ebe01bd49
johnnynode <johnnynode@gmail.com>
2017-11-18 18:59:30 +0800

    🍰 finished

---
+++ src/js/svg.js
@@ -0,0 +1,7 @@
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+
+// set width height
+cake = document.getElementById('cake-wrap');
+cake.setAttribute("width", SCREEN_WIDTH);
+cake.setAttribute("height", SCREEN_HEIGHT);
