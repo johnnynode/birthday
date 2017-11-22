c74b01207573698501b0e6a54896d7dcd8fbaf29
johnnynode <johnnynode@gmail.com>
2017-11-22 14:07:16 +0800

    make svg to cake

---
+++ src/js/cake.js
@@ -0,0 +1,8 @@
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+
+// set width height animation
+var cake = document.getElementById('cake-wrap');
+cake.setAttribute("width", SCREEN_WIDTH);
+cake.setAttribute("height", SCREEN_HEIGHT);
+cake.setAttribute('class', 'cur');
--- src/js/svg.js
+++
@@ -1,7 +0,0 @@
-var SCREEN_WIDTH = window.innerWidth;
-var SCREEN_HEIGHT = window.innerHeight;
-
-// set width height
-cake = document.getElementById('cake-wrap');
-cake.setAttribute("width", SCREEN_WIDTH);
-cake.setAttribute("height", SCREEN_HEIGHT);
