744dd58d8baa3655dc48ee0f25352135bc38e983
johnnynode <johnnynode@gmail.com>
2017-11-22 14:32:26 +0800

    make background change

--- src/js/cake.js
+++ src/js/cake.js
@@ -6,3 +6,11 @@ var cake = document.getElementById('cake-wrap');
 cake.setAttribute("width", SCREEN_WIDTH);
 cake.setAttribute("height", SCREEN_HEIGHT);
 cake.setAttribute('class', 'cur');
+
+// 变色处理
+var colorChangeCount = 0;
+cake.addEventListener('click',function() {
+  colorChangeCount++;
+  var arr = ['#3791f9','#fc183f','#343b44'];
+  document.body.style.backgroundColor = arr[colorChangeCount%3];
+},false)
